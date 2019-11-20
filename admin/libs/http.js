9e8bf3f9b0effce852baef71abbf7271a321a906
panjianwang <TomPan2012@163.com>
2019-11-20 21:00:18 +0800

    user页面功能完成
    对修改的数据进行了服务器更新，渲染，以及点击修改按钮后父页面也马上进行信息的更新

--- admin/libs/http.js
+++ admin/libs/http.js
@@ -3,22 +3,48 @@
   let url = {
     //   基地址
     ser_addr: ser_addr,
+    // 用户登录
     user_login: ser_addr + "/admin/user/login",
+    // 获取用户信息
     user_info: ser_addr + "/admin/user/info",
+    // 获取用户详情
     user_detail: ser_addr + "/admin/user/detail",
+    // 编辑用户信息
     user_edit: ser_addr + "/admin/user/edit",
-    user_list: ser_addr + "/admin/user/list",
-    user_add: ser_addr + "/admin/user/add",
-    user_search: ser_addr + "/admin/user/search",
-    user_delete: ser_addr + "/admin/user/delete",
-    user_query: ser_addr + "/admin/user/query",
-    user_publish: ser_addr + "/admin/user/publish",
-    user_info: ser_addr + "/admin/user/info",
-    user_article: ser_addr + "/admin/user/article",
-    user_category: ser_addr + "/admin/user/category",
-    user_visit: ser_addr + "/admin/user/visit",
-    user_pass: ser_addr + "/admin/user/pass",
-    user_reject: ser_addr + "/admin/user/reject"
+    // 文章类别
+    category_list: ser_addr + "/admin/category/list",
+    // 新增文章类别
+    category_add: ser_addr + "/admin/category/add",
+    // 根据id查询指定文章类别
+    category_search: ser_addr + "/admin/category/search",
+    // 编辑文章类别
+    categoty_edit: ser_addr + "/admin/categoty/edit",
+    // 删除文章类别
+    category_delete: ser_addr + "/admin/category/delete",
+    // 文章搜索
+    article_query: ser_addr + "/admin/article/query",
+    // 发布文章
+    article_publish: ser_addr + "/admin/article/publish",
+    // 根据id获取文章信息
+    article_search: ser_addr + "/admin/article/search",
+    // 文章编辑
+    article_edit: ser_addr + "/admin/article/edit",
+    // 删除文章
+    article_delete: ser_addr + "/admin/article/delete",
+    // 获取统计数据
+    data_info: ser_addr + "/admin/data/info",
+    // 日新增文章数量统计
+    data_article: ser_addr + "/admin/data/article",
+    // 各类型文章数量统计
+    data_category: ser_addr + "/admin/data/category",
+    // 日文章访问量
+    data_visit: ser_addr + "/admin/data/visit",
+    // 文章评论搜索
+    comment_search: ser_addr + "/admin/comment/search",
+    // 评论审核通过
+    comment_pass: ser_addr + "/admin/comment/pass",
+    // 评论审核不通过
+    comment_reject: ser_addr + "/admin/comment/reject"
   };
   w.url = url;
 })(window);
